ba6cfb4832309a9108f0a2850dde2868243f94c4
felipe <felipe.alves2014@gmail.com>
2025-07-14 21:50:56 -0300

    singleton

---
+++ creation/Singleton.py
@@ -0,0 +1,30 @@
+class Book:
+
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, title: str, price: str, cover: str):
+        if not hasattr(self, 'title'):
+            self.title = title
+            self.price = price
+            self.cover = cover
+
+    def get_title(self):
+        return self.title
+
+    def get_price(self):
+        return self.price
+    
+    def get_cover(self):
+        return self.cover
+    
+#test
+book1 = Book('book 1', 19.90, 'localhost:8080/fake-image.jpg')
+book2 = Book('book 2', 100.50, 'localhost:8080/fake-image2.jpg')
+print(book1.title)
+print(book2.title)
+print(book1 is book2)
